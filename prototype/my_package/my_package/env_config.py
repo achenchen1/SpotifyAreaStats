c49176b4a15e7aa2bdd9e9753078b4eb6e0b33b6
achenchen1 <alex.chen.chen@uwaterloo.ca>
2023-10-06 23:43:54 -0400

    Looking into just getting a package to manage all of this

---
+++ prototype/my_package/my_package/env_config.py
@@ -0,0 +1,7 @@
+import os
+
+# Load our common environment variables
+
+APP_CLIENT_ID = os.environ.get("APP_CLIENT_ID")
+APP_CLIENT_SECRET = os.environ.get("APP_CLIENT_SECRET")
+URI_DEFAULT = os.environ.get("URI_DEFAULT")
---
+++ prototype/my_package/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="my_package",
+    version="0.0.1",
+    author="Alex Chen",
+    author_email="alexcchen00@hotmail.com",
+    description="Package for prototyping Spotify endpoint",
+    packages=["my_package"],
+    install_requires=[],
+)
